94bb8e89d9f07fa991b1710a377140c610a46d7c
yoshikane takehiro <songs.of.the.sword@gmail.com>
2019-02-15 02:38:52 +0900

    名刺ー登録画面まで完成

--- app/controllers/meishis_controller.rb
+++ app/controllers/meishis_controller.rb
@@ -6,12 +6,19 @@ class MeishisController < ApplicationController
   end
 
   def new
-    
+    @meishi = Meishi.new
   end
 
   def edit
   end
-
-
-　
+  
+  def create
+    meishi = Meishi.new
+    meishi.save!
+    redirect_to meishis_url, #notice: "名刺名「#{meishi.name}」を登録しました"
+  end
+  private
+  def meishi_params
+    params.require(:task).permit(:name, :intro, :line, :insta, :twitter, :facebook)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'meishis/index'
+  get 'meishis/show'
+  get 'meishis/new'
+  get 'meishis/edit'
+  root to: 'meishis#index'
+  resources :meishis
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
