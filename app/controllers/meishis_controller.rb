94c62ccb020224299cceaf02145ea687e018e423
nomper <kenya.5528@gmail.com>
2019-03-13 00:34:06 +0900

    db構造を変更したのに合わせてmeishi/viewを変更した。

--- app/controllers/meishis_controller.rb
+++ app/controllers/meishis_controller.rb
@@ -4,7 +4,7 @@ class MeishisController < ApplicationController
       sql="SELECT * FROM meishis WHERE created_user==" << current_user.id.to_s
       @meishis = Meishi.find_by_sql(sql)
     else
-      redirect_to  "/forbidden", status: 301
+      redirect_to "/forbidden", status: 301
     end
   end
   
@@ -12,9 +12,9 @@ class MeishisController < ApplicationController
   def show
     @meishi = Meishi.find(params[:id]) #hash値からURLを付ける
     @url = params[:url]
-    if @meishi.url!=@url then 
+    if @meishi.key!=@url then 
       #実際は、その名刺へのアクセス権限がありません。的な静的ページを作る！forbidden.htmlなど
-      redirect_to  "/forbidden", status: 301
+      redirect_to "/forbidden", status: 301
     end
   end
 
@@ -25,7 +25,7 @@ class MeishisController < ApplicationController
   def edit
     @meishi = Meishi.find(params[:id])
     if @meishi.created_user!=current_user.id then 
-      redirect_to  "/forbidden", status: 301
+      redirect_to "/forbidden", status: 301
     end
   end
 
@@ -40,20 +40,20 @@ class MeishisController < ApplicationController
   def create
     @meishi = Meishi.new(meishi_params)
     @meishi.save
-    @meishi.url = Digest::MD5.hexdigest(@meishi.name*@meishi.id)
+    @meishi.key = Digest::MD5.hexdigest(@meishi.name*@meishi.id*current_user.id)
     @meishi.update(meishi_params)
     @meishi.created_user=current_user.id
     @meishi.update(meishi_params)
-    redirect_to  "/meishis/#{@meishi.id}/#{@meishi.url}/"
+    redirect_to "/meishis/#{@meishi.id}/#{@meishi.key}/"
   end
 
   def update
     @meishi = Meishi.find(params[:id])
     if @meishi.created_user!=current_user.id then 
-      redirect_to  "/forbidden", status: 301
+      redirect_to "/forbidden", status: 301
     else
       @meishi.update(meishi_params)
-      redirect_to  meishi_url, status: 301
+      redirect_to meishi_url, status: 301
     end
   end
 
@@ -63,6 +63,6 @@ class MeishisController < ApplicationController
   
   private
   def meishi_params
-    params.require(:meishi).permit(:name, :intro, :line, :insta, :twitter, :facebook) 
+    params.require(:meishi).permit(:name, :intro, :account1_info, :account2_info, :account3_info, :account4_info) 
   end
 end  
