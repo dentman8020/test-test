f40dd28d2b47d30d789170933ed5ac4fa38131ff
nomper <kenya.5528@gmail.com>
2019-03-13 00:02:25 +0900

    DB を新版に改築

--- db/migrate/20190214103508_create_meishis.rb
+++ db/migrate/20190214103508_create_meishis.rb
@@ -3,11 +3,19 @@ class CreateMeishis < ActiveRecord::Migration[5.2]
     create_table :meishis do |t|
       t.string :name
       t.text :intro
-      t.text :line
-      t.text :twitter
-      t.text :insta
-      t.text :facebook
-      t.text :url
+      t.text :account1_photo
+      t.text :account1_info
+      t.text :account1_display
+      t.text :account2_photo
+      t.text :account2_info
+      t.text :account2_display
+      t.text :account3_photo
+      t.text :account3_info
+      t.text :account3_display
+      t.text :account4_photo
+      t.text :account4_info
+      t.text :account4_display
+      t.text :key
       t.integer :created_user
       t.timestamps
     end
